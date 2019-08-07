80ba2e52b0405f3e8338da2907b0c7e8764ce75a
ly <450095923@qq.com>
2019-08-07 10:41:23 +0800

    .

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld123!");
         System.out.println(123);
-        System.out.println("idea112");
+        System.out.println("你是否有四个样子？一个是在朋友面前疯癫的样子，一个是在恋人面前完美的样子，一个是只身一人时脆弱的样子，还有一个，是在陌生的人群中安安静静的样子");
 
 
 
